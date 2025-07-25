c605667247f7209eb1af73e036a6e79e27334da7
reishi <reishi@topide.com>
2025-07-25 12:09:39 +0800

    Update install_base.sh

--- installers/ubuntu-20.04/install-partial/install_base.sh
+++ installers/ubuntu-20.04/install-partial/install_base.sh
@@ -46,20 +46,20 @@ service mysql start
 
 mkdir -p /usr/local/phyre/ssl
 
-wget https://raw.githubusercontent.com/PhyreApps/PhyrePanel/refs/heads/main/web/server/ssl/phyre.crt -O /usr/local/phyre/ssl/phyre.crt
-wget https://raw.githubusercontent.com/PhyreApps/PhyrePanel/refs/heads/main/web/server/ssl/phyre.key -O /usr/local/phyre/ssl/phyre.key
+wget https://raw.githubusercontent.com/ScriptElite/PhyrePanel/refs/heads/main/web/server/ssl/phyre.crt -O /usr/local/phyre/ssl/phyre.crt
+wget https://raw.githubusercontent.com/ScriptElite/PhyrePanel/refs/heads/main/web/server/ssl/phyre.key -O /usr/local/phyre/ssl/phyre.key
 
 sudo chmod 644 /usr/local/phyre/ssl/phyre.crt
 sudo chmod 600 /usr/local/phyre/ssl/phyre.key
 
-wget https://raw.githubusercontent.com/PhyreApps/PhyrePanel/main/installers/ubuntu-20.04/greeting.sh -O /etc/profile.d/phyre-greeting.sh
+wget https://raw.githubusercontent.com/ScriptElite/PhyrePanel/main/installers/ubuntu-20.04/greeting.sh -O /etc/profile.d/phyre-greeting.sh
 
 # Install PHYRE PHP
-wget https://github.com/PhyreApps/PhyrePanelPHP/raw/main/compilators/debian/php/dist/phyre-php-8.2.0-ubuntu-20.04.deb
+wget https://github.com/ScriptElite/PhyrePanelPHP/raw/main/compilators/debian/php/dist/phyre-php-8.2.0-ubuntu-20.04.deb
 dpkg -i phyre-php-8.2.0-ubuntu-20.04.deb
 
 # Install PHYRE NGINX
-wget https://github.com/PhyreApps/PhyrePanelNGINX/raw/main/compilators/debian/nginx/dist/phyre-nginx-1.24.0-ubuntu-20.04.deb
+wget https://github.com/ScriptElite/PhyrePanelNGINX/raw/main/compilators/debian/nginx/dist/phyre-nginx-1.24.0-ubuntu-20.04.deb
 dpkg -i phyre-nginx-1.24.0-ubuntu-20.04.deb
 
 PHYRE_PHP=/usr/local/phyre/php/bin/php
